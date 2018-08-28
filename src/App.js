b43176f88c5dde45d81d0c574d933da11d1ac853
kevin <kevin.leugers@gmail.com>
2018-08-28 01:37:18 -0400

    added parcel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-/* global React ReactDOM */
+import React from "react";
+import { render } from "react-dom";
+
 const Pet = props => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -33,4 +35,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));

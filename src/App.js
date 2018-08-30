d5e00cdcfdb35f61e2f2a44c9d4513f55b6dd279
Kevin Leugers <kevin+bit@talmetrix.com>
2018-08-30 03:25:01 -0400

    added details - end of day 1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router, Link } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-  componentDidMount() {
-    petfinder.pet.find({ output: "full", location: "Cincinnati, OH" }).then(data => {
-      let pets;
-
-      if (data.petfinder.pets && data.petfinder.pets.pet) {
-        if (Array.isArray(data.petfinder.pets.pet)) {
-          pets = data.petfinder.pets.pet;
-        } else {
-          pets = [data.petfinder.pets.pet];
-        }
-      } else {
-        pets = [];
-      }
-    });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me</h1>
-        <Pet name="Ghost" animal="Dog" breed="Husky" />
-        <Pet name="Summer" animal="Dog" breed="Husky" />
-        <Pet name="Nymeria" animal="Dog" breed="Husky" />
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
